da3eca144543c4df333d16090c7729ade1294793
guerni_t <guerni_t@etna-alternance.net>
2026-03-11 09:46:28 +0100

    added a user.py from ewen branch

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True)
+    password_hash = Column(String)
+    profile_picture = Column(String, nullable=True)
+    created_at = Column(DateTime, server_default=func.now())
+
+    clothings = relationship("Clothing", back_populates="user")
+    outfits = relationship("Outfit", back_populates="user")
