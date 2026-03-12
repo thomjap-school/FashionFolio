013297df0fe6c33400ce0c8e0dfaee635e2b132d
guerni_t <guerni_t@etna-alternance.net>
2026-03-12 23:29:11 +0100

    perf: Removed useless file

--- fashionfolio-backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Modèles SQLAlchemy pour la base de données."""
-
-from .clothing import Clothing
-
-__all__ = ["Clothing"]
--- fashionfolio-backend/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Routes de l'API."""
-
-from .chat import router as chat_router
-from .clothing import router as clothing_router
-
-__all__ = ["chat_router", "clothing_router"]
