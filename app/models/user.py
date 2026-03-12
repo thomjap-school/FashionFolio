11f8e63b998548f9ae53d503a6eb3cfa1c431fda
guerni_t <guerni_t@etna-alternance.net>
2026-03-12 22:38:01 +0100

    fix: comment relationships, fix bcrypt version

--- app/models/user.py
+++ app/models/user.py
@@ -3,16 +3,13 @@ from sqlalchemy.sql import func
 from app.core.database import Base
 from sqlalchemy.orm import relationship
 
-
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     username = Column(String, unique=True)
     password_hash = Column(String)
     profile_picture = Column(String, nullable=True)
     created_at = Column(DateTime, server_default=func.now())
-
-    clothings = relationship("Clothing", back_populates="user")
-    outfits = relationship("Outfit", back_populates="user")
+    # clothings = relationship("Clothing", back_populates="user")
+    # outfits = relationship("Outfit", back_populates="user")
