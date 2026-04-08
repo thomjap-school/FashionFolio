39090c1b366ef51e69873a3c8c50bd145e8eb43b
pernou_e <pernou_e@etna-alternance.net>
2026-04-08 08:40:39 +0200

    feat: add login as route

--- fashionfolio-front/src/App.tsx
+++ fashionfolio-front/src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Navigate to="/feed" replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/feed" element={<FeedPage />} />
         <Route path="/wardrobe" element={<WardrobePage />} />
         <Route path="/explore" element={<ExplorePage />} />
@@ -24,7 +24,7 @@ export default function App() {
         <Route path="/premium" element={<PremiumPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<Navigate to="/feed" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Layout>
   );
